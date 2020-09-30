969b1acc1a8c820dc44c26c0cabcf9c03af3c122
sarlinc <corentin.sarlin@etu.umontpellier.fr>
2020-09-30 10:40:01 +0200

    modif script + ajout de mysql

--- Step 02/script/script.js
+++ Step 02/script/script.js
@@ -1,135 +1,140 @@
 (function() {
-    let mysql = require('mysql');
+    const mysql = require('mysql');
 
-    let cnx = mysql.createConnection({
+    const cnx = mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "",
-        database: "test"
+        database: "jobboard"
     });
 
     function selectAll(table) {
         cnx.connect(function(err) {
-            try {
-                cnx.query(`SELECT * FROM ${table}`, function (err, result) {
-                    try {
-                        console.log('=======================');
-                        console.log(result); // doit être la liste
-
-                        console.log('=======================');
-                        for (res of result) {
-                            console.log(res.Titre);
-                        }
-                    }
-                    catch (err) {}
-
-                    cnx.end(function(err) {
-                        try {}
-                        catch (err) {}
-                    });
-                });
+            if (err) {
+                throw err;
             }
-            catch (err) {}
+
+            cnx.query(`SELECT * FROM ${table}`, function (err, result) {
+                if (err) {
+                    console.log('fail');
+                    throw err;
+                }
+                console.log('=======================');
+                console.log(result); // doit être la liste
+
+                console.log('=======================');
+
+                cnx.end(function(err) {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            });
         });
     }
 
     function selectById(table, id) {
         cnx.connect(function(err) {
-            try {
-                cnx.query(`SELECT ${id} FROM ${table} WHERE ${id}=${id}`, function (err, result) {
-                    try {
-                        console.log('=======================');
-                        console.log(result); // doit être la liste
-
-                        console.log('=======================');
-                        for (res of result) {
-                            console.log(res.Titre);
-                        }
-                    }
-                    catch (err) {}
-
-                    cnx.end(function(err) {
-                        try {}
-                        catch (err) {}
-                    });
-                });
+            if (err) {
+                throw err;
             }
-            catch (err) {}
+
+            cnx.query(`SELECT * FROM ${table} WHERE ${id}=${id}`, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                console.log('=======================');
+                console.log(result); // doit être la liste
+
+                console.log('=======================');
+
+                cnx.end(function(err) {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            });
         });
     }
 
     function del(id) {
         cnx.connect(function(err) {
-            try {
-                cnx.query(`SELECT ${id} FROM ${table} WHERE ${id}=${id}`, function (err, result) {
-                    try {
-                        console.log('=======================');
-                        console.log(result); // doit être la liste
-
-                        console.log('=======================');
-                        for (res of result) {
-                            console.log(res.Titre);
-                        }
-                    }
-                    catch (err) {}
-
-                    cnx.end(function(err) {
-                        try {}
-                        catch (err) {}
-                    });
-                });
+            if (err) {
+                throw err;
             }
-            catch (err) {}
+
+            cnx.query(`SELECT ${id} FROM ${table} WHERE ${id}=${id}`, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                console.log('=======================');
+                console.log(result); // doit être la liste
+
+                console.log('=======================');
+                for (res of result) {
+                    console.log(res.Titre);
+                }
+
+                cnx.end(function(err) {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            });
         });
     }
 
     function update() {
         cnx.connect(function(err) {
-            try {
-                cnx.query(`SELECT ${id} FROM ${table} WHERE ${id}=${id}`, function (err, result) {
-                    try {
-                        console.log('=======================');
-                        console.log(result); // doit être la liste
-
-                        console.log('=======================');
-                        for (res of result) {
-                            console.log(res.Titre);
-                        }
-                    }
-                    catch (err) {}
-
-                    cnx.end(function(err) {
-                        try {}
-                        catch (err) {}
-                    });
-                });
+            if (err) {
+                throw err;
             }
-            catch (err) {}
+
+            cnx.query(`SELECT ${id} FROM ${table} WHERE ${id}=${id}`, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                console.log('=======================');
+                console.log(result); // doit être la liste
+
+                console.log('=======================');
+                for (res of result) {
+                    console.log(res.Titre);
+                }
+
+                cnx.end(function(err) {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            });
         });
     }
 
     function create() {
         cnx.connect(function(err) {
-            try {
-                cnx.query(`SELECT ${id} FROM ${table} WHERE ${id}=${id}`, function (err, result) {
-                    try {
-                        console.log('=======================');
-                        console.log(result); // doit être la liste
-
-                        console.log('=======================');
-                        for (res of result) {
-                            console.log(res.Titre);
-                        }
-                    }
-                    catch (err) {}
-
-                    cnx.end(function(err) {
-                        try {}
-                        catch (err) {}
-                    });
-                });
+            if (err) {
+                throw err;
             }
-            catch (err) {}
+
+            cnx.query(`SELECT ${id} FROM ${table} WHERE ${id}=${id}`, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                console.log('=======================');
+                console.log(result); // doit être la liste
+
+                console.log('=======================');
+                for (res of result) {
+                    console.log(res.Titre);
+                }
+
+                cnx.end(function(err) {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            });
         });
     }
 })()
