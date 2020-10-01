11458e2e68fc0e7ef1b5c65ac9db0f959cddf3e6
sarlinc <corentin.sarlin@etu.umontpellier.fr>
2020-10-01 12:08:56 +0200

    ajout de axios

--- Step 04/frontend/script/script.js
+++ Step 04/frontend/script/script.js
@@ -1,3 +1,16 @@
 $(function() {
 
 });
+
+        /*
+            <li>
+                <div class="collapsible-header">
+                    <h5>Job name 1</h5>
+                    <h6 class="obj">My description in 2 words</h6>
+                    <p class="showmore">
+                        <a>learn more</a>
+                    </p>
+                </div>
+                <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+        */
