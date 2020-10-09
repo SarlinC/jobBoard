b7b96475dba23871d82d2ec5f6c67f0f9c7c4a40
ClovisSargenton <clovis.sargenton@gmail.com>
2020-10-09 11:33:12 +0200

    working on routes createPost

--- Step 04/backend/routes/createPost.js
+++ Step 04/backend/routes/createPost.js
@@ -6,14 +6,22 @@ let connection = require('../public/javascripts/phpMyAdmin');
 var router = express.Router();
 
 router.post('/', (req, res) => {
-    connection.query(`INSERT INTO advertisements (titre, objet, contenu, date, numCompanies)
-    VALUES ('${req.body.ad_title}', '${req.body.ad_object}', '${req.body.ad_contenu}', '${req.body.ad_date}')`, (err) => {
+
+    
+    connection.query(`SELECT numCompanies FROM companies 
+    WHERE numRecruteur=${req.body.ad_numPeople}`, (err , result) => {
         if (err) {
             throw err;
         }
-        else {
-            res.send('AD ADDED');
-        }
+        connection.query(`INSERT INTO advertisements (titre, objet, contenu, date, numCompanies)
+        VALUES ('${req.body.ad_title}', '${req.body.ad_object}', '${req.body.ad_contenu}', '${req.body.ad_date}', '${result[0].numCompanies}')`, (err) => {
+            if (err) {
+                throw err;
+            }
+            else {
+                res.send('AD ADDED');
+            }
+        });
     });
 });
 
