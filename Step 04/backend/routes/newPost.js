724935d54e444eed71a7d7171250a8ee94dda7f6
ClovisSargenton <clovis.sargenton@gmail.com>
2020-10-09 09:28:31 +0200

    working on back newPost

--- Step 04/backend/routes/newPost.js
+++ Step 04/backend/routes/newPost.js
@@ -4,3 +4,15 @@ const _ = require('lodash');
 let connection = require('../public/javascripts/phpMyAdmin');
 
 var router = express.Router();
+
+router.post('/', (req, res) => {
+    connection.query(`INSERT INTO advertisements (titre, objet, contenu, date, numCompanies)
+    VALUES ('${req.body.ad_title}', '${req.body.ad_object}', '${req.body.ad_contenu}', '${req.body.ad_date}')`, (err) => {
+        if (err) {
+            throw err;
+        }
+        else {
+            res.send('AD ADDED');
+        }
+    });
+});
--- Step 04/frontend/script/newPost.js
+++ Step 04/frontend/script/newPost.js
@@ -5,13 +5,13 @@ $(function () {
         let ad_title = $('#ad_title').val();
         let ad_object = $('#ad_object').val();
         let ad_contenu = $('#ad_contenu').val();
-        let now_date = Date(Date.now()).slice(0, 25);
+        let ad_date = Date(Date.now()).slice(0, 25);
 
         axios.post('http://localhost:3000/newPost', {
             ad_title: ad_title,
             ad_object: ad_object,
             ad_contenu: ad_contenu,
-            now_date: now_date
+            ad_date: ad_date
         }).then(response => {
             console.log(response.data);
         }).catch(err => {
