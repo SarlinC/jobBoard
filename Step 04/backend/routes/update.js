66ad5368109737a161bf5ae2a3a856a2ae7154d7
sarlinc <corentin.sarlin@etu.umontpellier.fr>
2020-10-09 11:49:59 +0200

    ajout de la fonctionnalité d'update des ad

--- Step 04/backend/routes/update.js
+++ Step 04/backend/routes/update.js
@@ -1,5 +1,7 @@
 var express = require('express');
 
+let _ = require('lodash');
+
 let connection = require('../public/javascripts/phpMyAdmin');
 
 var router = express.Router();
@@ -7,13 +9,23 @@ var router = express.Router();
 /* POST registering user. */
 
 
-router.post('/', function(req, res) {
-    connection.query(`SELECT * FROM advertisements WHERE numAdvertisements='${req.body.numAdvertisements}'`, (err, result) => {
-        if(err) {
-            throw err;
-        }
-        res.send(result);
-    });
+router.post('/', function (req, res) {
+    if (_.isEmpty(req.body.titre)) {
+        connection.query(`SELECT * FROM advertisements WHERE numAdvertisements='${req.body.numAdvertisements}'`, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            res.send(result);
+        });
+    }
+    else {
+        connection.query(`UPDATE advertisements SET titre='${req.body.titre}', objet='${req.body.objet}', contenu='${req.body.contenu}', date='${Date(Date.now()).slice(0, 25)}'
+            WHERE numAdvertisements='${req.body.numAdvertisements}'`, err => {
+            if (err) {
+                throw err;
+            }
+        });
+    }
 });
 
 module.exports = router;
