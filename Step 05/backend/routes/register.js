ac0640670eb30d920d66e5dd95f1e67573453259
ClovisSargenton <clovis.sargenton@gmail.com>
2020-10-12 09:45:55 +0200

    changed isRecruteur to isAdmin in back

--- Step 05/backend/routes/register.js
+++ Step 05/backend/routes/register.js
@@ -11,8 +11,8 @@ var router = express.Router();
 
 router.post('/', function(req, res) {
     bcrypt.hash(req.body.pwd_string, 10).then(hash => {
-        connection.query(`INSERT INTO people (nomPeople, prenomPeople, emailPeople, password, isRecruteur)
-        VALUES ('${req.body.lastName}', '${req.body.firstName}', '${req.body.email_string}', '${hash}', '${req.body.is_recruteur}')`, (err) => {
+        connection.query(`INSERT INTO people (nomPeople, prenomPeople, emailPeople, password, isAdmin)
+        VALUES ('${req.body.lastName}', '${req.body.firstName}', '${req.body.email_string}', '${hash}', '${req.body.isAdmin}')`, (err) => {
             if(err) {
                 throw err;
             }
